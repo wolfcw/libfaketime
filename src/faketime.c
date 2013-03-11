776ee94a3f104b97e1b8ddbaf13409afbcca5147
Suan-Aik Yeo <syeo@enova.com>
2013-03-11 00:37:19 -0500

    basic version working

--- src/faketime.c
+++ src/faketime.c
@@ -23,6 +23,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <attr/xattr.h>
 
 /* pthread-handling contributed by David North, TDI in version 0.7 */
 #ifdef PTHREAD
@@ -645,6 +646,10 @@ time_t fake_time(time_t *time_tptr) {
     static int cache_expired = 1;       /* considered expired at first call */
     static int cache_duration = 10;     /* cache fake time input for 10 seconds */
 
+    static char rcfile_touched_xattr[BUFFERLEN]; 
+    char xattr_buf[BUFFERLEN];
+    int xattr_size;
+
 #ifdef LIMITEDFAKING
     static long callcounter = 0;
     static int limited_initialized = 0;
@@ -764,6 +769,14 @@ static pthread_mutex_t time_mutex=PTHREAD_MUTEX_INITIALIZER;
 #ifdef NO_CACHING
     cache_expired = 1;
 #endif
+
+    if ((xattr_size = getxattr("/etc/faketimerc", "user.last_touched", xattr_buf, BUFFERLEN)) != -1) {
+        xattr_buf[xattr_size] = 0;
+        if (strcmp(rcfile_touched_xattr, xattr_buf) != 0) {
+            cache_expired = 1;
+            strcpy(rcfile_touched_xattr, xattr_buf);
+        }
+    }
 
     if (cache_expired == 1) {
 
