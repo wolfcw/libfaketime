e09350e374dc9791a0aaa4f81309123c0ed61a94
Wolfgang Hommel <wolfgang.hommel@lrz.de>
2013-09-04 14:21:13 +0200

    Applied libfaketime name change and style guide

--- src/faketime_common.h
+++ src/faketime_common.h
@@ -3,20 +3,20 @@
  *
  * Copyright 2013 Balint Reczey <balint@balintreczey.hu>
  *
- * This file is part of the FakeTime Preload Library.
+ * This file is part of the libfaketime.
  *
- * The FakeTime Preload Library is free software; you can redistribute it
- * and/or modify it under the terms of the GNU General Public License v2 as
- * published by the Free Software Foundation.
+ * libfaketime is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v2 as published by the Free
+ * Software Foundation.
  *
- * The FakeTime Preload Library is distributed in the hope that it will
- * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * libfaketime is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
  *
- * You should have received a copy of the GNU General Public License v2
- * along with the FakeTime Preload Library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * You should have received a copy of the GNU General Public License v2 along
+ * with libfaketime; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #ifndef FAKETIME_COMMON_H
@@ -24,25 +24,26 @@
 
 #include <stdint.h>
 
-struct system_time_s {
-  /** System time according to CLOCK_REALTIME */
+struct system_time_s
+{
+  /* System time according to CLOCK_REALTIME */
   struct timespec real;
-  /** System time according to CLOCK_MONOTONIC */
+  /* System time according to CLOCK_MONOTONIC */
   struct timespec mon;
-  /** System time according to CLOCK_MONOTONIC_RAW */
+  /* System time according to CLOCK_MONOTONIC_RAW */
   struct timespec mon_raw;
 };
 
-
-/** Data shared among faketime-spawned processes */
-struct ft_shared_s {
-  /**
+/* Data shared among faketime-spawned processes */
+struct ft_shared_s
+{
+  /*
    * When advancing time linearly with each time(), etc. call, the calls are
    * counted here */
   uint64_t ticks;
-  /** Index of timstamp to be loaded from file */
+  /* Index of timstamp to be loaded from file */
   uint64_t file_idx;
-  /** System time Faketime started at */
+  /* System time Faketime started at */
   struct system_time_s start_time;
 };
 
