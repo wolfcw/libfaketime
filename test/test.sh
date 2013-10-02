bb87fb619e01814ec9930eeb8dd0e62ee2cdff0b
Wolfgang Hommel <wolfgang.hommel@lrz.de>
2013-10-02 15:32:46 +0200

    improved readability of test script output

--- test/test.sh
+++ test/test.sh
@@ -13,6 +13,7 @@ else
 fi
 
 echo "============================================================================="
+echo
 
 echo "Running the test program with absolute date 2003-01-01 10:00:05 specified"
 echo "\$ LD_PRELOAD=../src/libfaketime.so.1 FAKETIME=\"2003-01-01 10:00:05\" ./timetest"
@@ -20,6 +21,7 @@ LD_PRELOAD=../src/libfaketime.so.1 FAKETIME="2003-01-01 10:00:05" ./timetest
 echo
 
 echo "============================================================================="
+echo
 
 echo "Running the test program with START date @2005-03-29 14:14:14 specified"
 echo "\$ LD_PRELOAD=../src/libfaketime.so.1 FAKETIME=\"@2005-03-29 14:14:14\" ./timetest"
@@ -27,6 +29,7 @@ LD_PRELOAD=../src/libfaketime.so.1 FAKETIME="@2005-03-29 14:14:14" ./timetest
 echo
 
 echo "============================================================================="
+echo
 
 echo "Running the test program with 10 days negative offset specified"
 echo "LD_PRELOAD=../src/libfaketime.so.1 FAKETIME=\"-10d\" ./timetest"
@@ -34,6 +37,7 @@ LD_PRELOAD=../src/libfaketime.so.1 FAKETIME="-10d" ./timetest
 echo
 
 echo "============================================================================="
+echo
 
 echo "Running the test program with 10 days negative offset specified, and FAKE_STAT disabled"
 echo "\$ LD_PRELOAD=../src/libfaketime.so.1 FAKETIME=\"-10d\" NO_FAKE_STAT=1 ./timetest"
@@ -41,6 +45,7 @@ LD_PRELOAD=../src/libfaketime.so.1 FAKETIME="-10d" NO_FAKE_STAT=1 ./timetest
 echo
 
 echo "============================================================================="
+echo
 
 echo "Running the test program with 10 days postive offset specified, and sped up 2 times"
 echo "\$ LD_PRELOAD=../src/libfaketime.so.1 FAKETIME=\"+10d x2\" ./timetest"
@@ -48,6 +53,7 @@ LD_PRELOAD=../src/libfaketime.so.1 FAKETIME="+10d x2" NO_FAKE_STAT=1 ./timetest
 echo
 
 echo "============================================================================="
+echo
 
 echo "Running the 'date' command with 15 days negative offset specified"
 echo "\$ LD_PRELOAD=../src/libfaketime.so.1 FAKETIME=\"-15d\" date"
