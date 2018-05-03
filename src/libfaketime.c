8107db78491dfcc1609540ba9517c3b90cf0a1f1
Wolfgang Hommel <wolfgang.hommel@unibw.de>
2018-05-03 19:36:47 +0200

    Improved parsing error logging

--- src/libfaketime.c
+++ src/libfaketime.c
@@ -1581,6 +1581,7 @@ static void parse_ft_string(const char *user_faked_time)
       else
       {
         perror("Failed to parse FAKETIME timestamp");
+        fprintf(stderr, "Please check specification %s with format %s\n", user_faked_time, user_faked_time_fmt);
         exit(EXIT_FAILURE);
       }
       break;
