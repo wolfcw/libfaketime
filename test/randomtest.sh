0b0cc29d2e6593f73e1d54eb983b57c8109a7288
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2021-02-25 21:56:51 -0500

    test/randomtest.sh: avoid touching the filesystem

--- test/randomtest.sh
+++ test/randomtest.sh
@@ -6,15 +6,13 @@ set -e
 
 error=0
 
-FAKERANDOM_SEED=0xDEADBEEFDEADBEEF LD_PRELOAD="$FTPL" ./repeat_random 3 5 > repeat3x5 
-FAKERANDOM_SEED=0xDEADBEEFDEADBEEF LD_PRELOAD="$FTPL" ./repeat_random 5 3 > repeat5x3
+repeat3x5="$(FAKERANDOM_SEED=0xDEADBEEFDEADBEEF LD_PRELOAD="$FTPL" ./repeat_random 3 5)"
+repeat5x3="$(FAKERANDOM_SEED=0xDEADBEEFDEADBEEF LD_PRELOAD="$FTPL" ./repeat_random 5 3)"
 
-if ! diff -u repeat3x5 repeat5x3; then
-    error=5
-    printf >&2 '5 calls of getrandom(3) did not produce the same stream as 3 calls of getrandom(5)\n'
+if [ "$repeat3x5" != "$repeat5x3" ]; then
+    error=1
+    printf >&2 '5 calls of getrandom(3) got %s\n3 calls of getrandom(5) got %s\n' "$repeat3x5" "$repeat5x3"
 fi
-
-rm -f repeat3x5 repeat5x3
 
 if [ 0 = $error ]; then
     printf 'getrandom interception test successful.\n'
