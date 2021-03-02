a92d6ffe7c2bb867bdbc71d6a24dcadb3798f2db
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2021-03-02 10:23:11 -0500

    add snippet testing clock_gettime(CLOCK_REALTIME)

---
+++ test/snippets/clock_gettime.c
@@ -0,0 +1,8 @@
+struct timespec ts;
+clockid_t ckid = CLOCK_REALTIME;
+int ret = clock_gettime(ckid, &ts);
+if (ret == 0) {
+  printf("[%s] clock_gettime(CLOCK_REALTIME[%d], &ts) -> {%lld, %ld}\n", where, ckid, (long long)ts.tv_sec, ts.tv_nsec);
+ } else {
+  printf("[%s] clock_gettime(CLOCK_REALTIME[%d], &ts) returned non-zero (%d), errno = %d (%s)\n", where, ckid, ret, errno, strerror(errno));
+ }
