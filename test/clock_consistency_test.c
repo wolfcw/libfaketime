58717c085ba48e57ae33a4b388537ea69cf3ab61
Wolfgang Hommel <wolfgang.hommel@unibw.de>
2026-08-30 00:09:57 +0200

    Check monotonic clock ordering

--- test/clock_consistency_test.c
+++ test/clock_consistency_test.c
@@ -8,6 +8,9 @@ int main(void)
   time_t seconds = time(NULL);
   struct timeval timeval_value;
   struct timespec timespec_value;
+  struct timespec previous_monotonic;
+  struct timespec current_monotonic;
+  int sample;
 
   if (gettimeofday(&timeval_value, NULL) != 0 ||
       clock_gettime(CLOCK_REALTIME, &timespec_value) != 0)
@@ -22,6 +25,23 @@ int main(void)
   {
     fprintf(stderr, "clock APIs returned inconsistent values\n");
     return EXIT_FAILURE;
+  }
+  if (clock_gettime(CLOCK_MONOTONIC, &previous_monotonic) != 0)
+  {
+    perror("monotonic clock probe");
+    return EXIT_FAILURE;
+  }
+  for (sample = 0; sample < 3; sample++)
+  {
+    if (clock_gettime(CLOCK_MONOTONIC, &current_monotonic) != 0 ||
+        current_monotonic.tv_sec < previous_monotonic.tv_sec ||
+        (current_monotonic.tv_sec == previous_monotonic.tv_sec &&
+         current_monotonic.tv_nsec < previous_monotonic.tv_nsec))
+    {
+      fprintf(stderr, "monotonic clock moved backwards\n");
+      return EXIT_FAILURE;
+    }
+    previous_monotonic = current_monotonic;
   }
   puts("clock APIs agreed");
   return EXIT_SUCCESS;
